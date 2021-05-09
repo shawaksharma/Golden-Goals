3177912ee52e6ad4a3bcc92b8c2a0f9a85309e37
Shawak Sharma <s.sharma@newcastle.ac.uk>
2021-05-09 22:46:39 +0100

    Initial Commit

---
+++ Golden Goals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Golden Goals
+//
+//  Created by Shawak Sharma on 09/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Golden Goals/Golden_GoalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Golden_GoalsApp.swift
+//  Golden Goals
+//
+//  Created by Shawak Sharma on 09/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Golden_GoalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
